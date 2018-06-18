4a7bfcbce45a67d46b054e2645a68e8c6859248b
the-loner <37229727+the-loner@users.noreply.github.com>
2018-06-19 01:28:25 +0200

    Creazione searchbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import CardList from './CardList';
-
+import { robots } from './robots';
+import SearchBox from './SearchBox';
 
 
 const App = () => {
-return();
+return(
+    <div>
+    <h1>RoboFriends</h1>
+    <SearchBox/>
+    <CardList robots={robots} /> 
+    </div>
+);
 }
+
+export default App;
