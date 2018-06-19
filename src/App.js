57ba3e1706610f93e03358940aa4089ae2a3164a
the-loner <sandro.palladino@gmail.com>
2018-06-19 15:58:22 +0200

    sto aggungendo la searchbox ma ho errori

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
 
-const App = () => {
-return(
-    <div>
-    <h1>RoboFriends</h1>
-    <SearchBox/>
-    <CardList robots={robots} /> 
-    </div>
-);
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+onSearchChange (event) {
+    
+    const filteredRobots=this.state.robots.filter(robots => {
+        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    console.log(filteredRobots);
+}
+
+    render() {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots} />
+            </div>
+             );
+            }
 }
 
 export default App;
